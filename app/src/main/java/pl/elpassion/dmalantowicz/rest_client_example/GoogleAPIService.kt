ce8e085ceeeade22e9041881f43af8fa7f639da6
dmalantowicz <dominik.malantowicz@elpassion.pl>
2016-01-18 14:47:19 +0100

    Calling Api via Observable

--- app/src/main/java/pl/elpassion/dmalantowicz/rest_client_example/GoogleAPIService.kt
+++ app/src/main/java/pl/elpassion/dmalantowicz/rest_client_example/GoogleAPIService.kt
@@ -1,9 +1,9 @@
 package pl.elpassion.dmalantowicz.rest_client_example
 
 import pl.elpassion.dmalantowicz.rest_client_example.domain.PlaceListWrapper
-import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
+import rx.Observable
 
 /**
  * Created by dmalantowicz on 18.01.2016.
@@ -11,6 +11,6 @@ import retrofit2.http.Query
 interface GoogleAPIService {
 
     @GET("maps/api/place/nearbysearch/json?location=52.2398936,20.9880451&radius=5000&&key=%20AIzaSyC8Cl3TYbzkZ6bb8_fwKeMhFvx_Be6B0CY")
-    fun places (@Query("name") name : String) : Call<PlaceListWrapper>
+    fun places (@Query("name") name : String) : Observable<PlaceListWrapper>
 
 }
